d9b24596fa7a298174a14c6b3d8776b020b3af8f
Norman Scheurig <norman.scheurig@gmail.com>
2024-09-26 12:01:44 +0200

    Update Website

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,12 @@
-
-
 const HomePage = () => {
   return (
-    <section className="relative h-screen flex items-center justify-center bg-fixed bg-center bg-cover custom-img">
-      {/* Hintergrundbild */}
+    <section className="relative w-full h-screen bg-fixed bg-center bg-cover custom-img flex items-center justify-center">
+      {/* Overlay für besseren Kontrast */}
       <div className="absolute top-0 left-0 w-full h-full bg-black opacity-50 z-0"></div>
-      
+
       <div className="container relative z-10 text-center text-white">
         <h1 className="text-5xl font-bold mb-6 animate-slideIn">Willkommen bei Ihrem Steuerberater</h1>
-        <p className="text-lg mb-4">
+        <p className="text-lg mb-8">
           Wir bieten umfassende Steuerberatung und Dienstleistungen für Unternehmen und Privatpersonen.
         </p>
         <button className="bg-cta text-white py-2 px-4 rounded hover:bg-cta-hover transition">
