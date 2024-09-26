418cb4eea91a72a604d87cb2d2340eaae648f19d
Norman Scheurig <norman.scheurig@gmail.com>
2024-09-27 00:09:11 +0200

    Update kontakt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,11 @@ import Link from 'next/link';
 
 const HomePage = () => {
   return (
-    <section className="relative w-full h-screen bg-fixed bg-center bg-cover custom-img flex items-center justify-between">
+    <section className="relative w-full h-screen bg-fixed bg-center bg-cover custom-img">
       {/* Overlay for contrast */}
-      <div className="absolute top-0 left-0 w-full h-full bg-black opacity-0 z-0"></div>
 
-      {/* Left Section with Welcome Text */}
-      <div className="container relative z-10 text-white text-left ml-10">
+      {/* Left Section with Welcome Text - Vertically centered and 25% from left */}
+      <div className="absolute top-[40%] left-0 transform -translate-y-1/2 relative z-10 text-white text-left">
         <h1 className="text-5xl font-bold mb-6 animate-slideIn transition-transform duration-300 hover:scale-105">
           Willkommen bei Ihrem Steuerberater
         </h1>
@@ -23,8 +22,8 @@ const HomePage = () => {
         </Link>
       </div>
 
-      {/* Right Section with Kanzlei Card - Positioned further down, wider, and less tall with hover effect */}
-      <div className="absolute bottom-8 right-16 bg-white bg-opacity-90 text-gray-900 p-4 rounded-lg shadow-lg w-80 h-48 flex flex-col justify-between transition-transform duration-300 hover:scale-105 hover:shadow-xl">
+      {/* Kanzlei Card - Positioned at 60% from the top and 60% from the left */}
+      <div className="absolute top-[70%] left-[80%] transform -translate-x-1/2 -translate-y-1/2 bg-white bg-opacity-90 text-gray-900 p-4 rounded-lg shadow-lg w-80 h-48 flex flex-col justify-between transition-transform duration-300 hover:scale-105 hover:shadow-xl">
         <div>
           <h2 className="text-xl font-semibold mb-2 text-center">Kanzlei</h2>
           <p className="text-sm text-gray-700 mb-2 text-center">
